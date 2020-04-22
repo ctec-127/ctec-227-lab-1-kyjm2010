eea2459efdc21310960910e2e440bc8fea9ce75d
Kyle McDonald <kyjm2010@gmail.com>
2020-04-22 12:33:41 -0700

    Ready for grading by Bruce

--- destroy_all_sessions.php
+++ destroy_all_sessions.php
@@ -1,15 +1,3 @@
-<!-- On the destroy_all_session.php page do the following:
-
-- Ensure that your page starts with a session_start().
-- Write PHP code to destroy the entire user session using session_destory().
-- At the bottom of the page place 3 links:
-  - A link to the create_session.php page
-  - A link to the read_session.php page
-  - A link to the destroy_session.php page
-  - Go to the create_session.php page to ensure that the session variables are set.
-- Go to the read_session.php page to ensure that the variables were in fact destroyed.
-- Now click on the link to the destroy_all_session.php to destroy the session.
-- Go back to the read page and observe what has happened. -->
 <?php 
     session_start();
     session_destroy();
--- read_session.php
+++ read_session.php
@@ -1,14 +1,3 @@
-<!-- On the read_session.php page do the following:
-
-- Ensure that your page starts with a session_start()
-- Create PHP code to read and display the session variables.
-- At the bottom of the page place 3 links:
-  - A link to the create_session.php page
-  - A link to the destroy_session.php page
-  - A link to the destroy_all_session.php page
-- Test that the session variables are being set by going back to the create_session.php page and reloading it a couple of times.
-- Then go back to the read_session.php page to ensure that they are being updated. -->
-
 <?php 
     session_start();
     echo "<p>" . $_SESSION['views'] . "</p>";
